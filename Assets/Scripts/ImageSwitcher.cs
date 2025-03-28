0abbf663f36cac0e449b73956772a732af20f3ec
jingxuawu3 <jingxuawu3-c@my.cityu.edu.hk>
2025-03-27 23:29:39 -0700

    add image delete ui

---
+++ Assets/Scripts/ImageSwitcher.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ImageSwitcher : MonoBehaviour
+{
+    public Image image; 
+    public Sprite secondImage;
+    private float switchDelay = 8f; 
+    private float disappearDelay = 5f; 
+
+    void Start()
+    {
+        StartCoroutine(SwitchImage());
+    }
+
+    private IEnumerator SwitchImage()
+    {
+        yield return new WaitForSeconds(switchDelay); 
+        image.sprite = secondImage; 
+
+        yield return new WaitForSeconds(disappearDelay); 
+        image.gameObject.SetActive(false); 
+    }
+}
