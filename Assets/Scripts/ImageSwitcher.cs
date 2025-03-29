a38d927043ba60f827028f31c8e4b56f87209400
jingxuawu3 <jingxuawu3-c@my.cityu.edu.hk>
2025-03-29 01:50:48 -0700

    change tutorial and add intro scene

--- Assets/Scripts/ImageSwitcher.cs
+++ Assets/Scripts/ImageSwitcher.cs
@@ -4,11 +4,12 @@ using System.Collections;
 
 public class ImageSwitcher : MonoBehaviour
 {
-    public Image image; 
+    public Image image;
     public Sprite secondImage;
-    private float switchDelay = 8f; 
-    private float disappearDelay = 5f; 
-
+    public Sprite thirdImage; 
+    private float switchDelay = 19f;
+    private float disappearDelay = 5f;
+    private float disappearDelayDoor = 4f;
     void Start()
     {
         StartCoroutine(SwitchImage());
@@ -16,10 +17,16 @@ public class ImageSwitcher : MonoBehaviour
 
     private IEnumerator SwitchImage()
     {
-        yield return new WaitForSeconds(switchDelay); 
-        image.sprite = secondImage; 
+       
+        yield return new WaitForSeconds(switchDelay);
+        image.sprite = secondImage;
 
+      
         yield return new WaitForSeconds(disappearDelay); 
+        image.sprite = thirdImage;
+
+      
+        yield return new WaitForSeconds(disappearDelayDoor);
         image.gameObject.SetActive(false); 
     }
 }
