fa1bd547929b424454aa263051704bfa4f826f01
yixiaoli <yixiaoli@umich.edu>
2025-04-20 22:42:52 -0600

    fix: fixed tutorial image display

--- Assets/Scripts/ImageSwitcher.cs
+++ Assets/Scripts/ImageSwitcher.cs
@@ -7,9 +7,9 @@ public class ImageSwitcher : MonoBehaviour
     public Image image;
     public Sprite secondImage;
     public Sprite thirdImage; 
-    private float switchDelay = 19f;
-    private float disappearDelay = 5f;
-    private float disappearDelayDoor = 4f;
+    private float switchDelay = 12f;
+    private float disappearDelay = 14f;
+    private float disappearDelayDoor = 6f;
     void Start()
     {
         StartCoroutine(SwitchImage());
