b439fcc136f0c4f4f3831bbc0ce7c99ba217fa75
shujiejune <jieniu.june@tutanota.com>
2025-05-02 04:19:56 -0700

    fixed gate collision issue

--- Assets/Scripts/GateCollision.cs
+++ Assets/Scripts/GateCollision.cs
@@ -12,16 +12,42 @@ public class GateCollision : MonoBehaviour
     public Obstacle obstacleManager;
     public CameraShake cameraShake;
 
+    private void Start()
+{   
+    if (playerController == null)
+    {
+        playerController = FindObjectOfType<PlayerController>();
+        if (playerController == null)
+            Debug.LogError("PlayerController not found!");
+    }
+
+    if (obstacleManager == null)
+    {
+        obstacleManager = FindObjectOfType<Obstacle>();
+        if (obstacleManager == null)
+            Debug.LogError("ObstacleManager not found!");
+    }
+
+}
+
     private void OnCollisionEnter(Collision collision)
     {
         // Check if the object colliding with the obstacle is the ball.
         if (collision.gameObject.CompareTag("Player"))
         {
+            Debug.Log("Player hit the gate: " + gameObject.name);
+            Debug.LogError("PlayerController is null:" + playerController == null);
+            
             if(playerController != null) {
+                Debug.Log("Decrement 10 balls because of gate collision.");
                 obstacleManager.ShowFloatingText("-10");
                 playerController.ApplyPenalty(-10);
             }
-
+/*          
+            Debug.Log("Decrement 10 balls because of gate collision.");
+            obstacleManager.ShowFloatingText("-10");
+            playerController.ApplyPenalty(-10);
+*/
             cameraShake = FindObjectOfType<CameraShake>();
             if (cameraShake == null)
             {
